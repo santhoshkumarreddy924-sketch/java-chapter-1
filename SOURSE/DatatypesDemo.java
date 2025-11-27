36b3198e98717ff0a4ba827ba651e6054a29cf75
santhoshkumarreddy924-sketch <santhoshkumarreddy924@gmail.com>
2025-11-27 12:44:54 +0530

    Create DatatypesDemo.java

---
+++ SOURSE/DatatypesDemo.java
@@ -0,0 +1,13 @@
+class DataTypesDemo {
+    public static void main(String[] args) {
+        int age = 20;
+        double marks = 88.5;
+        char grade = 'A';
+        boolean pass = true;
+
+        System.out.println(age);
+        System.out.println(marks);
+        System.out.println(grade);
+        System.out.println(pass);
+    }
+}
