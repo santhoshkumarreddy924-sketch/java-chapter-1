e762f9668c01847f87d4fe6f39a66a5b5e2edd0d
santhoshkumarreddy924-sketch <santhoshkumarreddy924@gmail.com>
2025-11-27 12:43:52 +0530

    Create ConstantDemo.java

---
+++ SOURSE/ConstantDemo.java
@@ -0,0 +1,9 @@
+class ConstantExample {
+    public static void main(String[] args) {
+        final double PI = 3.14;
+        int r = 5;
+        double area = PI * r * r;
+
+        System.out.println("Area = " + area);
+    }
+}
